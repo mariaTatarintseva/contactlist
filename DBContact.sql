f728ff61535fd9b46c5707e675913032b6a04eb7
unknown <m.tatarintseva.mt@gmail.com>
2015-03-12 08:26:48 +0300

    second commit

--- DBContact.sql
+++ DBContact.sql
@@ -29,11 +29,11 @@ CREATE TABLE `adress` (
   `place` int(10) unsigned DEFAULT NULL,
   `post_index` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8;
 
 /*Data for the table `adress` */
 
-insert  into `adress`(`id`,`country`,`town`,`street`,`house`,`place`,`post_index`) values (1,'Bel1','Gomel','Kirova',11,22,NULL),(2,NULL,NULL,NULL,NULL,NULL,NULL),(3,'Bel','Mnsk','Kirova',12,11,NULL),(4,'','','',NULL,NULL,NULL),(5,'','','',NULL,NULL,NULL),(6,'','','',NULL,NULL,NULL),(7,'','','',NULL,NULL,NULL),(8,'','','',NULL,NULL,NULL),(9,'','','',NULL,NULL,NULL),(10,'','','',NULL,NULL,NULL),(11,'','','',NULL,NULL,NULL);
+insert  into `adress`(`id`,`country`,`town`,`street`,`house`,`place`,`post_index`) values (56,'Belarus','Minsk','Kirova',NULL,NULL,220030),(57,'','','',NULL,NULL,NULL),(58,'','Moscow','',NULL,NULL,NULL),(59,'France','Paris',NULL,NULL,NULL,NULL),(60,NULL,NULL,NULL,NULL,NULL,NULL),(61,'','St. Petersburg','',NULL,NULL,NULL),(62,'','','',NULL,NULL,NULL),(63,'','','',NULL,NULL,NULL),(64,'','','',NULL,NULL,NULL),(65,'','','',NULL,NULL,NULL),(66,'','','',NULL,NULL,NULL);
 
 /*Table structure for table `attachment` */
 
@@ -47,11 +47,11 @@ CREATE TABLE `attachment` (
   `CONTACT_ID` int(11) DEFAULT NULL,
   `PATH` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `attachment` */
 
-insert  into `attachment`(`ID`,`NAME`,`DATE`,`COMMENT`,`CONTACT_ID`,`PATH`) values (1,'12','2015-03-10 00:04:48','',17,'D:\\itechart\\project\\ContactList\\target\\ContactList\\attachments\\2015-03-10288052\\bb.TXT');
+insert  into `attachment`(`ID`,`NAME`,`DATE`,`COMMENT`,`CONTACT_ID`,`PATH`) values (1,'12','2015-03-10 00:04:48','',17,'D:\\itechart\\project\\ContactList\\target\\ContactList\\attachments\\2015-03-10288052\\bb.TXT'),(2,'Name','2015-03-12 05:53:07','Comment',50,'C:\\Temp\\attachments-2015-03-1221187651\\MyMain.txt');
 
 /*Table structure for table `contact` */
 
@@ -68,16 +68,18 @@ CREATE TABLE `contact` (
   `EMAIL` varchar(20) DEFAULT NULL,
   `adress_id` int(10) unsigned DEFAULT NULL,
   `GENDER` enum('MALE','FEMALE') DEFAULT NULL,
-  `FAMILY` enum('SINGLE','MARRIED','DATING','DIVORCED','WIDOW') DEFAULT NULL,
+  `FAMILY` enum('SINGLE','MARRIED','DATING','DIVORCED','WIDOW','NOT_SPECIFIED') DEFAULT NULL,
+  `CITIZENSHIP` varchar(15) DEFAULT NULL,
+  `WEBSITE` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `ID` (`ID`),
   KEY `contact_ibfk_1` (`adress_id`),
   CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`adress_id`) REFERENCES `adress` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 
 /*Data for the table `contact` */
 
-insert  into `contact`(`ID`,`NAME`,`SURNAME`,`FATHERNAME`,`BIRTHDAY`,`JOB`,`PHOTO`,`EMAIL`,`adress_id`,`GENDER`,`FAMILY`) values (10,'Petya','Petrov','',NULL,'',NULL,'',1,NULL,NULL),(14,'NotNotPetr','NotPetrov','',NULL,'',NULL,'',NULL,NULL,NULL),(17,'aaa','qqq-q','','1010-09-12','','avatars/2015-03-0981978066/x_54HmV7Czs.jpg','',NULL,NULL,NULL),(21,'Sergey','Sergeew','','1991-03-12','',NULL,'',NULL,NULL,NULL),(22,'Petrov','Petya1','',NULL,'',NULL,'',NULL,NULL,NULL),(23,'NewContact','Contact','New',NULL,'',NULL,'',NULL,NULL,NULL),(24,'trert','qwewq','',NULL,'',NULL,'',NULL,NULL,NULL),(25,'CNewC','CCNewCC','','1991-02-12','',NULL,'',11,NULL,NULL),(26,'Name','Surnam','',NULL,'','','123',NULL,NULL,NULL),(27,'qweweq','asdsa','',NULL,'',NULL,'',NULL,NULL,NULL),(28,'Qweewq','asdad','',NULL,'',NULL,'sdfasf',NULL,NULL,NULL),(29,'ertre',' ddvd ','',NULL,'',NULL,'',NULL,NULL,NULL),(30,'qweq','sfdsf','',NULL,'',NULL,' d ad',NULL,NULL,NULL),(31,'qwewq','sdfds','',NULL,'',NULL,'',NULL,NULL,NULL),(33,'rtyuytr','ertytre','',NULL,'',NULL,'',NULL,NULL,NULL),(34,'NewNewNEW','0703','','1234-05-10','',NULL,'qwe@qwe.ru',NULL,NULL,NULL),(35,'qwe','asd','',NULL,'',NULL,'',NULL,NULL,NULL),(39,'NewContact','Contact2309','','1990-08-21','','','email@mail.com',NULL,NULL,NULL);
+insert  into `contact`(`ID`,`NAME`,`SURNAME`,`FATHERNAME`,`BIRTHDAY`,`JOB`,`PHOTO`,`EMAIL`,`adress_id`,`GENDER`,`FAMILY`,`CITIZENSHIP`,`WEBSITE`) values (49,'Ivan','Ivanov',NULL,'1990-01-12','BSU',NULL,'email1@contacts.com',56,'MALE','MARRIED',NULL,NULL),(50,'Petr','Petrov',NULL,'1990-01-10',NULL,NULL,NULL,57,'MALE','NOT_SPECIFIED',NULL,NULL),(51,'Elena','Sidorova',NULL,NULL,NULL,NULL,'elena@abc.com',58,'FEMALE','DIVORCED',NULL,NULL),(52,'Pierre','Levacances',NULL,NULL,'IBM',NULL,'pierre@gmail.com',59,'MALE','','french',NULL),(53,'Serge','Levacances',NULL,NULL,'IBM',NULL,'pierre@gmail.com',60,'MALE','','french',NULL),(54,'Vasily','Vasilev',NULL,NULL,NULL,NULL,NULL,61,'MALE','DATING',NULL,NULL),(55,'Nicolas','Sasev',NULL,NULL,NULL,NULL,NULL,62,'MALE','NOT_SPECIFIED',NULL,NULL),(56,'Ernst','Ernst',NULL,'1988-04-10',NULL,NULL,NULL,63,'MALE','NOT_SPECIFIED',NULL,NULL),(57,'Irina','Petrova',NULL,NULL,'McDonald\'s',NULL,'mashuga.08@mail.ru',64,'FEMALE','MARRIED','McDonald\'s',NULL),(58,'Natalie','Kuznetsova',NULL,NULL,NULL,NULL,NULL,65,'MALE','NOT_SPECIFIED',NULL,NULL),(59,'Yahn','Yahnkovsky',NULL,NULL,'student',NULL,'yahn@mail.by',66,'MALE','SINGLE',NULL,NULL);
 
 /*Table structure for table `phone` */
 
@@ -94,11 +96,9 @@ CREATE TABLE `phone` (
   KEY `id` (`id`),
   KEY `contact_id` (`contact_id`),
   CONSTRAINT `contact_id` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 /*Data for the table `phone` */
-
-insert  into `phone`(`id`,`number`,`type`,`comment`,`contact_id`,`country`,`operator`) values (11,'+316934','CELL','Comment',10,NULL,NULL),(12,'+31629934','CELL','Comment2',10,NULL,NULL),(17,'3','HOME','Comment',39,'1','2'),(18,'123','HOME','',25,'1','2'),(19,'321','CELL','',25,'123','12'),(20,'12','HOME','Ð¹',25,'12','123'),(21,'5','CELL','zz',25,'23','234');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
